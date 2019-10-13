a895c767ab24bf0deab6cc71392bbd4f04511b90
Freitas29 <arao.freitas@hotmail.com>
2019-10-13 12:30:59 -0300

    Added function to open modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-document.getElementById('card').addEventListener('click', () => {
-    alert("testando")
+document.getElementById('card-button').addEventListener('click', () => {
+    let card = document.getElementById('card')
+
+    if(card.style.display === "block")
+        card.style.display = "none"
+    else
+        card.style.display = "block"
 })
