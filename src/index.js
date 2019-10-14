d7b795fd29e28c50eb4931de05e162f89b498698
Freitas29 <arao.freitas@hotmail.com>
2019-10-14 01:49:59 -0300

    Funcionalidade para ir até a api e buscar os dados do carrinho

--- src/index.js
+++ src/index.js
@@ -6,3 +6,26 @@ document.getElementById('card-button').addEventListener('click', () => {
     else
         card.style.display = "block"
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadDoc()
+})
+
+
+const loadDoc = () => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        buildCard(this.responseText)
+      }
+    };
+    xhttp.open("GET", "https://raw.githubusercontent.com/Freitas29/challenge-front/master/products.json", true);
+    xhttp.send(); 
+}
+
+const buildCard = (response) => {
+    let result = convertToJSON(response)
+    console.log(result)
+}
+
+const convertToJSON = value => JSON.parse(value)
