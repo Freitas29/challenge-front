d2b9eb5ed984cdc978b9cd4695805b006c2340f3
Freitas29 <arao.freitas@hotmail.com>
2019-10-14 03:07:31 -0300

    label to qtd added

--- src/index.js
+++ src/index.js
@@ -20,6 +20,11 @@ const buildListItems = data => {
     let main = divRow.appendChild(buildColums("col-lg-8"))
     let mainTitle = main.appendChild(buildColums("col-lg-12"))
     mainTitle.appendChild(buildTitle(data.cart.item[0].name))
+
+    let mainDescription =  main.appendChild(buildColums("col-lg-12"))
+    let rowDescription = mainDescription.appendChild(buildRow("align-items-center"))
+    let colDescription = rowDescription.appendChild(buildColums("col-lg-4"))
+    colDescription.appendChild(buildLabelQtd(data.cart.item[0].quantity))
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -48,15 +53,19 @@ const convertToJSON = value => JSON.parse(value)
 
 
 // Contrutores dos elementos
-const buildRow = () => {
+const buildRow = (value = false) => {
     let element =  document.createElement('div')
     element.classList.add('row')
     element.style.padding = 0
     element.style.margin = 0
+    if(value){
+        element.classList.add("d-flex")
+        element.classList.add(value)
+    }
     return element
 }
 
-const buildColums = (value,padding) => {
+const buildColums = (value,padding = true) => {
     let element =  document.createElement('div')
     element.classList.add(value)
     if(padding){
@@ -77,5 +86,20 @@ const buildTitle = value => {
     let element = document.createElement('P')
     element.classList.add("title")
     element.innerText = value
+    return element
+}
+
+const buildLabelMoney = value => {
+    let element = document.createElement('LABEL')
+    element.classList.add("description-money")
+    element.innerText = value
+    return element
+}
+
+const buildLabelQtd = value => {
+    let element = document.createElement('LABEL')
+    element.classList.add("description")
+    element.classList.add("strong")
+    element.innerText = `Qtd: ${value}`
     return element
 }
